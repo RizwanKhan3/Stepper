8559d2457d97c42b989089887c27746215adfdaa
Rizwan <rizwankh750@gmail.com>
2023-11-22 17:07:17 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Stepper } from './components/Stepper.jsx'
+
+function App() {
+  return (
+    <div className='m-auto w-full'>
+      <Stepper />
+    </div>
+  )
+}
+
+export default App
